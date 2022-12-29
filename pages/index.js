8084b6f2394bf2dbe7be872ad3f5724cc1246cc3
Brian Neeland <brian.neeland@protonmail.com>
2022-12-28 17:49:59 -0700

    Add link to app on main page

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        Main content div
+        <Link href="app">Start</Link>
       </div>
     </>
   )
