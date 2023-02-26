4f849d50117ebadbed2067af81e31678d6390d31
Brian Neeland <brian.neeland@protonmail.com>
2023-02-25 17:36:48 -0700

    Implement interactive forms for participants page

--- components/ui/button.js
+++ components/ui/button.js
@@ -5,7 +5,7 @@ const COLORS = {
 
 export default function Button({ icon, label, callback, color }) {
   return (
-    <button onClick={callback} className={`${COLORS[color]} hover:bg-slate-300 px-4 py-2 rounded-2xl border-b-4 active:border-b-0 active:border-t-4 border-slate-300 hover:border-slate-400 flex items-center gap-2 transition-colors`}>
+    <button onClick={callback} className={`${COLORS[color]} hover:bg-slate-300 px-4 py-2 rounded-2xl border-b-4 active:border-b-0 active:border-t-4 border-slate-300 hover:border-slate-400 flex items-center gap-3 transition-colors`}>
       <div className="text-slate-400">{icon}</div>
       <div className="text-slate-800">{label}</div>
     </button>
